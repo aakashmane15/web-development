8963596d681fa6ee29272b9ac3bb7738a071a506
Aakash Mane <aakashmane5058@gmail.com>
2026-09-15 12:12:26 +0530

    feat: add prisma and connected a database

--- week-19/week-19-next-app/app/api/v1/signup/route.ts
+++ week-19/week-19-next-app/app/api/v1/signup/route.ts
@@ -1,8 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prismaClient = new PrismaClient({ adapter });
 
 export async function POST(req: NextRequest) {
   const data = await req.json();
   console.log(data);
+
+  await prismaClient.user.create({
+    data: {
+      username: data.username,
+      password: data.password,
+    },
+  });
 
   return NextResponse.json({
     message: "You have been signed-up",
